2fd62fc3a566c041f20f618adeae98bbba98f676
Mona Lisa <yourEmail>
2024-04-10 20:07:21 +0330

    Complete the password_input

--- lib/view/Register/signup.dart
+++ lib/view/Register/signup.dart
@@ -17,69 +17,122 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: mco,
-                      borderRadius: const BorderRadius.vertical(
-                        bottom: Radius.circular(25),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: mco,
+                    borderRadius: const BorderRadius.vertical(
+                      bottom: Radius.circular(25),
                     ),
-                    height: 170,
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'obrazec',
-                        ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  height: 170,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'obrazec',
                       ),
                     ),
                   ),
-                  const SizedBox(height: 25),
-                  Form(
-                    child: Column(
-                      children: [
+                ),
+                const SizedBox(height: 25),
+                Form(
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Username",
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
                       Username_Input(),
                       SizedBox(
-                        height: 32,
+                        height: 16,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Email",
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
                       ),
                       Email_Input(),
                       SizedBox(
-                        height: 32,
+                        height: 16,
                       ),
-                        Password_Input(),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Phone Number",
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
                       SizedBox(
-                        height: 32,
+                        height: 20,
                       ),
-                        Phonenumber_Input(),
-                        // Add more fields as needed
-                      ],
-                    ),
+                      Phonenumber_Input(),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Password",
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Password_Input(),
+
+                      // Add more fields as needed
+                    ],
+
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
+      ),
     );
-
   }
 }
